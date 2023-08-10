9efb1a6355228be0e4ad318f58337c30e4790b71
JPSE <watrasinarith@gmail.com>
2023-08-10 18:59:19 +0700

    CODE

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    if y==0:
+        return "ERROR: Division By Zero 555!!"
+    return x/y
+print("Welcome to Python Calculator!!")
+print("Select Operation: ")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1,2,3,4): ")
+if choice not in ['1','2','3','4']:
+    print("Invalid choice")
+else:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second numberL: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    if choice == '2':
+        print("Result: ", subtract(num1,num2))
+    if choice == '3':
+        print("Result", multiply(num1, num2))
+    if choice =='4':
+        print("Result: ", divide(num1, num2))
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Test-repo
  My awesome movement!
+ This is from Rith Sinawatra(JPSE)
+# Calculator Project
+
+This is a basic calculator application that can perform addition, subtraction, multiplication, and division. It's a command-line calculator written in Python.
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## Getting Started
+
+To use this calculator, you need to have Python installed on your system. If you don't have Python installed, you can download it from the official website: [Python Download](https://www.python.org/downloads/).
+
+
